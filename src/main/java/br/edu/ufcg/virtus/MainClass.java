fecc0dbc93df218ffa56c90b1437d1ceaa402a1a
Jota Freitas Jr <jotafreitasjr@gmail.com>
2018-11-29 10:53:41 -0300

    Project initial commit

---
+++ src/main/java/br/edu/ufcg/virtus/MainClass.java
@@ -0,0 +1,13 @@
+package br.edu.ufcg.virtus;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainClass
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/br/edu/ufcg/virtus/MainClassTest.java
@@ -0,0 +1,20 @@
+package br.edu.ufcg.virtus;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple MainClass.
+ */
+public class MainClassTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
